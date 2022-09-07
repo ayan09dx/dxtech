c6a9e3467e337b686ee0bb506b883ee4cac845ca
UAT_UXDC_SBI1 <uat_uxdc_sbi1@GCBUAT1D112.local>
2022-09-07 18:23:44 +0530

    mid

--- components/Imageslider.js
+++ components/Imageslider.js
@@ -1,11 +1,11 @@
+/* eslint-disable @next/next/no-img-element */
 import React from 'react'
 
 export default function Imageslider() {
   return (
    <>
-    <video className='homevideo' autoPlay muted loop>
-       <source src='/homevid.mp4' type="video/mp4" />
-    </video>
+   <img src="/bannerback.gif" alt="banner" className='homevideo'/>
+    
     <div className='video_container'>
     </div>
   </>
